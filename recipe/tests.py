b960049ab41ae7409b2c8cc31c627f15f01e6cd4
Artem <artem4sav@gmail.com>
2026-05-22 13:18:12 +0300

    Add setup and unit test for Recipe creation

--- recipe/tests.py
+++ recipe/tests.py
@@ -7,3 +7,18 @@ class CategoryModelTest(TestCase):
         category = Category.objects.create(name="Desserts")
 
         self.assertEqual(category.name, "Desserts")
+
+class RecipeModelTest(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name="Main Course")
+
+    def test_recipe_creation(self):
+        recipe = Recipe.objects.create(
+            title="Pasta",
+            description="Delicious pasta",
+            instructions="Boil water, add pasta...",
+            ingredients="Pasta, salt, water",
+            category=self.category
+        )
+        self.assertEqual(recipe.title, "Pasta")
+        self.assertEqual(recipe.category.name, "Main Course")
